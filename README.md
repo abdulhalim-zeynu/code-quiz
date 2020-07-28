b216a5de7645edae43ecee37510ac43811aab5d1
Abdulhalim Zeynu <a.zeynu8@gmail.com>
2020-07-27 19:41:02 -0500

    completed HW

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This Code Quiz is meant to test your coding knowledge and compare your high score with friends.
+Good luck!
---
+++ questions.js
@@ -0,0 +1,32 @@
+var jsQuestions = [
+  {
+    title: "Which of the following is not a JSON type?",
+    choices: ["Object", "date", "Array", "string"],
+    answer: "date"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Is JavaScript fun to work with?",
+    choices: ["No", "Sometimes", "What is Javascript", "Not just yes, but HELL YES!"],
+    answer: "Not just yes, but HELL YES!"
+  },
+  {
+    title: "DOM is an abreviation for ____",
+    choices: ["Data Object Mode", "Dumb Old Man", "Document Object Model", "Dutle Opo Mipsy"],
+    answer: "Document Object Model"
+  },
+  {
+    title: "______ tag is an extension to HTML that can enclose any number of JavaScript",
+    choices: ["<SCRIPT>", "<BODY>", "<HEAD>", "<TITLE>"],
+    answer: "<SCRIPT>"
+  },
+  {
+    title: "What are variables used for in JavaScript Programs?",
+    choices: ["Storing numbers, dates, or other values", "Varying randomly", "Causing high-school algebra flashbacks", " None of the above"],
+    answer: "Storing numbers, dates, or other values"
+  }
+];
